83f176541ef499ec0ed6688f69cd8aa046f14ff1
mike-axiom-mir <miketobi90@gmail.com>
2026-09-21 01:55:27 +0200

    chore: expose Form 0.15.0 candidate version

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const {
   validateComposeIntentKeys,
   normalizeMixedComposition
 } = require("./mixed-composition");
-const MACHINE = { id: "axm.morphtile.machine.form", version: "0.14.0" };
+const MACHINE = { id: "axm.morphtile.machine.form", version: "0.15.0" };
 
 function holdResult(request, hold, suggested_missing_capability = null) {
   return result(request, MACHINE, "HOLD", {
