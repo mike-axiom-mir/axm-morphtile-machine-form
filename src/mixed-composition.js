8b91e24d5937436af4c43dfc1a95d74c4732fcf9
mike-axiom-mir <miketobi90@gmail.com>
2026-09-20 22:57:59 +0200

    feat: support repeat size progression in compose

--- src/mixed-composition.js
+++ src/mixed-composition.js
@@ -5,7 +5,7 @@ const {
   normalizeDefinitionInstances,
   normalizeGrid
 } = require("./form-vocabulary");
-const { normalizeRepeatWithRotation } = require("./repeat-rotation");
+const { normalizeRepeatWithSize } = require("./repeat-size");
 
 const COMPOSE_ITEM_KEYS = new Set(["part", "instance", "repeat", "grid"]);
 const COMPOSE_INTENT_KEYS = new Set(["name", "compose"]);
@@ -65,7 +65,7 @@ function normalizeMixedComposition(items) {
       data = instance.data[0];
       usesDefinition = true;
     } else if (mode === "repeat") {
-      const repeated = normalizeRepeatWithRotation(item.repeat);
+      const repeated = normalizeRepeatWithSize(item.repeat);
       if (!repeated.ok) return hold(repeated.hold.code, `${label}.repeat: ${repeated.hold.detail}`);
       data = repeated.data;
       placements = repeated.data.repeat;
