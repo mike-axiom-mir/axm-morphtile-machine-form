33b6ba7c27aee5fc97221cc01a7d0b9b6a06aae7
mike-axiom-mir <miketobi90@gmail.com>
2026-09-20 23:56:11 +0200

    feat: reuse bounded scale progression in compose

--- src/mixed-composition.js
+++ src/mixed-composition.js
@@ -5,7 +5,7 @@ const {
   normalizeDefinitionInstances,
   normalizeGrid
 } = require("./form-vocabulary");
-const { normalizeRepeatWithSize } = require("./repeat-size");
+const { normalizeRepeatWithScale } = require("./repeat-scale");
 
 const COMPOSE_ITEM_KEYS = new Set(["part", "instance", "repeat", "grid"]);
 const COMPOSE_INTENT_KEYS = new Set(["name", "compose"]);
@@ -65,7 +65,7 @@ function normalizeMixedComposition(items) {
       data = instance.data[0];
       usesDefinition = true;
     } else if (mode === "repeat") {
-      const repeated = normalizeRepeatWithSize(item.repeat);
+      const repeated = normalizeRepeatWithScale(item.repeat);
       if (!repeated.ok) return hold(repeated.hold.code, `${label}.repeat: ${repeated.hold.detail}`);
       data = repeated.data;
       placements = repeated.data.repeat;
